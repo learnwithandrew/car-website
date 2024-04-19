4722b51c8bf28e2fc268ced9c275dfc60f43c042
andyjohn23 <andrewnick906@gmail.com>
2024-04-19 09:09:00 +0300

    add comments

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let cars = ["station wagon", "hatchback", "convertible", "suv", "minivan", "pickup truck", "sedan", "sports car", "coupe", "electric", "luxury", "hybrid"]
+
 function searchCar(searchItem) {
-    for (let carItem = 0; carItem < cars.length; carItem++) {
+    for (let carItem = 0; carItem < cars.length; carItem = carItem + 1) {
         if (cars[carItem] === searchItem) {
             return `You searched for ${cars[carItem].toUpperCase()} and its index ${carItem} in the cars array.`;
         }
@@ -8,12 +9,11 @@ function searchCar(searchItem) {
     return `The item ${searchItem.toUpperCase()} was not found in the cars stack!`;
 }
 
-
 function sortCars(carsCopy) {
     carsCopy = cars.slice();
     carsCopy.sort();
     return carsCopy;
 }
 
-console.log(searchCar('suv suv'));
+console.log(searchCar('suv'));
 console.log(sortCars(cars));
