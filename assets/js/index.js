d0369c69e65576006e342799f082a7ea7d2bc1d5
andyjohn23 <andrewnick906@gmail.com>
2024-05-10 13:39:29 +0300

    add dom manipulation

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,29 +1,50 @@
-let cars = [
-    {
-        id: 1,
-        carName: "Sports car",
-        carImage: "./assets/images/sports-car_prev_ui.png",
-        carDetails: "this is a sports car"
-    },
-    {
-        id: 2,
-        carName: "Convertible car",
-        carImage: "./assets/images/convertible_prev_ui.png",
-        carDetails: "this is a convertible car"
-    },
-    {
-        id: 3,
-        carName: "Minivan car",
-        carImage: "./assets/images/minivan_prev_ui.png",
-        carDetails: "this is a minivan car"
-    },
-    {
-        id: 4,
-        carName: "Electric car",
-        carImage: "./assets/images/tesla_prev_ui.png",
-        carDetails: "this is an electric car"
+document.addEventListener('DOMContentLoaded', () => {
+    let carContainer = document.getElementById('car-cards');
+    let cars = [
+        {
+            id: 1,
+            carName: "Convertible car",
+            carImage: "./assets/images/convertible_prev_ui.png",
+            carDetails: "this is a convertible car"
+        },
+        {
+            id: 2,
+            carName: "Sports car",
+            carImage: "./assets/images/sports-car_prev_ui.png",
+            carDetails: "this is a sports car"
+        },
+        {
+            id: 3,
+            carName: "Minivan car",
+            carImage: "./assets/images/minivan_prev_ui.png",
+            carDetails: "this is a minivan car"
+        },
+        {
+            id: 4,
+            carName: "Electric car",
+            carImage: "./assets/images/tesla_prev_ui.png",
+            carDetails: "this is an electric car"
+        }
+    ]
+
+    function displayCars() {
+        cars.forEach((car, index) => {
+            const carCard = document.createElement('div');
+            carCard.classList.add('card');
+            carCard.innerHTML = `
+          <img src="${car.carImage}" alt="${car.carName}" />
+          <h5>${car.carName}</h5>
+          <a href="${car.carName}/${index}" class="shop-now-link">Shop now</a>
+        `
+            console.log(carCard)
+            carContainer.appendChild(carCard)
+        })
     }
-]
+
+    displayCars();
+
+})
+
 // function that searches through the cars array
 function searchCar(searchItem) {
     for (let carItem = 0; carItem < cars.length; carItem = carItem + 1) {
---
+++ dom.js
@@ -0,0 +1,13 @@
+// let newElement = document.createElement('p');
+// newElement.textContent = "I am learning DOM manipulation in Javascript";
+// document.querySelector('body').appendChild(newElement)
+
+// let secondLi = document.getElementById('second-li').textContent='thank you DOm manipulation'
+// console.log(secondLi)
+
+let clickButton = document.getElementById('button');
+clickButton.addEventListener('click', () => {
+    const message = 'Yea!!, I am learning DOM manipulation';
+    alert(message);
+    console.log(message);
+})
