3327de57f11516ed6677b93d7921fde2bc83dbe9
andyjohn23 <andrewnick906@gmail.com>
2024-05-14 21:14:50 +0300

    refactor code, add modal to display individual car items

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,66 +3,58 @@ document.addEventListener('DOMContentLoaded', () => {
     let cars = [
         {
             id: 1,
-            carName: "Convertible car",
-            carImage: "./assets/images/convertible_prev_ui.png",
-            carDetails: "this is a convertible car"
+            carName: "Sports car",
+            carImage: "/assets/images/sports-car_prev_ui.png",
+            carDetails: "This is a sports car."
         },
         {
             id: 2,
-            carName: "Sports car",
-            carImage: "./assets/images/sports-car_prev_ui.png",
-            carDetails: "this is a sports car"
+            carName: "Electric car",
+            carImage: "/assets/images/tesla_prev_ui.png",
+            carDetails: "This is an electric car."
         },
         {
             id: 3,
             carName: "Minivan car",
-            carImage: "./assets/images/minivan_prev_ui.png",
-            carDetails: "this is a minivan car"
+            carImage: "/assets/images/minivan_prev_ui.png",
+            carDetails: "This is a minivan car."
         },
         {
             id: 4,
-            carName: "Electric car",
-            carImage: "./assets/images/tesla_prev_ui.png",
-            carDetails: "this is an electric car"
+            carName: "Convertible car",
+            carImage: "/assets/images/convertible_prev_ui.png",
+            carDetails: "This is a convertible car."
         }
     ]
 
-    function displayCars() {
-        cars.forEach((car, index) => {
-            const carCard = document.createElement('div');
+    let displayCars = () => {
+        let carClick = document.getElementById('myModal');
+        let carClose = document.getElementById('modal-close');
+        let modalInfo = document.getElementById('modal-info')
+
+        cars.map((car) => {
+            let carCard = document.createElement('div');
             carCard.classList.add('card');
             carCard.innerHTML = `
-          <img src="${car.carImage}" alt="${car.carName}" />
-          <h5>${car.carName}</h5>
-          <a href="${car.carName}/${index}" class="shop-now-link">Shop now</a>
-        `
-            console.log(carCard)
-            carContainer.appendChild(carCard)
+             <img src="${car.carImage}" alt="${car.carName}" />
+             <h5>${car.carName}</h5>
+            `;
+            carContainer.appendChild(carCard);
+            let pickItem = () => {
+                carCard.onclick = () => {
+                    carClick.style.display = "block";
+                    modalInfo.innerHTML = `
+                    <img src="${car.carImage}" alt="${car.carName}" />
+                    <h5>${car.carName}</h5>
+                    `;
+                };
+                carClose.onclick = () => {
+                    carClick.style.display = "none";
+                }
+            }
+            pickItem(car.id);
         })
     }
 
     displayCars();
-
-})
-
-// function that searches through the cars array
-function searchCar(searchItem) {
-    for (let carItem = 0; carItem < cars.length; carItem = carItem + 1) {
-        if (cars[carItem] === searchItem) {
-            return `You searched for ${cars[carItem].toUpperCase()} and its index ${carItem} in the cars array.`;
-        }
-    }
-
-    return `The item ${searchItem} was not found in the cars stack!`;
-};
-
-// function that sorts the cars array
-function sortedCars(carsCopy) {
-    carsCopy = cars.slice();
-    carsCopy.sort();
-};
-
-
-let searchedCar = searchCar('suv');
-console.log(searchedCar);
-// console.log(sortCars(cars));
+});
