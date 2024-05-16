e5e21d35590e91970c4169ee1ef3c21f36714288
andyjohn23 <andrewnick906@gmail.com>
2024-05-16 07:50:02 +0300

    add fetch capability, data from db.json and update card layout to grid

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,32 +1,32 @@
 document.addEventListener('DOMContentLoaded', () => {
     let carContainer = document.getElementById('car-cards');
     // let categorySection = document.getElementById('category-section');
-    let cars = [
-        {
-            id: 1,
-            carName: "Sports car",
-            carImage: "/assets/images/sports-car_prev_ui.png",
-            carDetails: "This is a sports car."
-        },
-        {
-            id: 2,
-            carName: "Electric car",
-            carImage: "/assets/images/tesla_prev_ui.png",
-            carDetails: "This is an electric car."
-        },
-        {
-            id: 3,
-            carName: "Minivan car",
-            carImage: "/assets/images/minivan_prev_ui.png",
-            carDetails: "This is a minivan car."
-        },
-        {
-            id: 4,
-            carName: "Convertible car",
-            carImage: "/assets/images/convertible_prev_ui.png",
-            carDetails: "This is a convertible car."
-        }
-    ]
+    // let cars = [
+    //     {
+    //         id: 1,
+    //         carName: "Sports car",
+    //         carImage: "/assets/images/sports-car_prev_ui.png",
+    //         carDetails: "This is a sports car."
+    //     },
+    //     {
+    //         id: 2,
+    //         carName: "Electric car",
+    //         carImage: "/assets/images/tesla_prev_ui.png",
+    //         carDetails: "This is an electric car."
+    //     },
+    //     {
+    //         id: 3,
+    //         carName: "Minivan car",
+    //         carImage: "/assets/images/minivan_prev_ui.png",
+    //         carDetails: "This is a minivan car."
+    //     },
+    //     {
+    //         id: 4,
+    //         carName: "Convertible car",
+    //         carImage: "/assets/images/convertible_prev_ui.png",
+    //         carDetails: "This is a convertible car."
+    //     }
+    // ]
 
     let displayCars = () => {
         let carClick = document.getElementById('myModal');
@@ -34,32 +34,36 @@ document.addEventListener('DOMContentLoaded', () => {
         let carImage = document.getElementById('modalImage-container');
         let carInformation = document.getElementById('carModal-information')
         // let carReferenceHeader = document.getElementById('carReference-heading');
-
-        cars.map((car) => {
-            let carCard = document.createElement('div');
-            carCard.classList.add('card');
-            carCard.innerHTML = `
-             <img src="${car.carImage}" alt="${car.carName}" />
-             <h5>${car.carName}</h5>
-            `;
-            let pickItem = () => {
-                let openCarModal = () => {
-                    carClick.style.display = "block";
-                    carImage.innerHTML = `<img src="${car.carImage}" alt="${car.carName}" />`;
-                    carInformation.innerHTML = `
-                        <h5>${car.carName}</h5>
-                        <p>${car.carDetails}</p>
+        fetch('http://localhost:3000/cars')
+            .then(response => response.json())
+            .then((data) => {
+                let cars = data;
+                cars.map((car) => {
+                    let carCard = document.createElement('div');
+                    carCard.classList.add('card');
+                    carCard.innerHTML = `
+                     <img src="${car.carImage}" alt="${car.carName}" />
+                     <h5>${car.carName}</h5>
                     `;
-                };
-                let closeCarModal = () => {
-                    carClick.style.display = "none";
-                }
-                carCard.addEventListener('click', openCarModal);
-                carClose.addEventListener('click', closeCarModal);
-            }
-            pickItem(car.id);
-            carContainer.appendChild(carCard);
-        })
+                    let pickItem = () => {
+                        let openCarModal = () => {
+                            carClick.style.display = "block";
+                            carImage.innerHTML = `<img src="${car.carImage}" alt="${car.carName}" />`;
+                            carInformation.innerHTML = `
+                                <h5>${car.carName}</h5>
+                                <p>${car.carDetails}</p>
+                            `;
+                        };
+                        let closeCarModal = () => {
+                            carClick.style.display = "none";
+                        }
+                        carCard.addEventListener('click', openCarModal);
+                        carClose.addEventListener('click', closeCarModal);
+                    }
+                    pickItem(car.id);
+                    carContainer.appendChild(carCard);
+                })
+            })
     }
     displayCars();
 });
---
+++ asynch.js
@@ -0,0 +1,9 @@
+// console.log("before");
+// setTimeout(() => console.log("during"), 8000);
+// console.log("after");
+
+// let data = () => {
+//     fetch('http://localhost:3000/cars').then(response => console.log(response))
+// }
+
+// data();
