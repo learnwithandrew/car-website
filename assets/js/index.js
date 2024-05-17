2f9d7a514ca1520faea217a0400c1c4560b8fa6a
andyjohn23 <andrewnick906@gmail.com>
2024-05-17 10:07:28 +0300

    create delete car functionality

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,12 +4,11 @@ document.addEventListener('DOMContentLoaded', () => {
     let carFormDetails = document.getElementById('car-details');
     let carFormName = document.getElementById('car-name');
     let carForm = document.getElementById('carSubmit-form');
+    let carModal = document.getElementById('myModal')
 
     let displayAllCars = () => {
         let carClick = document.getElementById('myModal');
         let carClose = document.getElementById('modal-close');
-        let carImage = document.getElementById('modalImage-container');
-        let carInformation = document.getElementById('carModal-information');
 
         fetch('http://localhost:3000/cars')
             .then(response => response.json())
@@ -24,16 +23,26 @@ document.addEventListener('DOMContentLoaded', () => {
                         `;
                     let pickItem = () => {
                         let openCarModal = () => {
+                            let container = document.getElementById('modal-info');
                             carClick.style.display = "block";
-                            carImage.innerHTML = `<img src="${car.carImage}" alt="${car.carName}" />`;
-                            carInformation.innerHTML = `
+                            container.innerHTML =
+                                `<div class="image-container">
+                                    <img src="${car.carImage}" />
+                                 </div>
+                                 <div class="car-information">
                                     <h5>${car.carName}</h5>
                                     <p>${car.carDetails}</p>
+                                 </div>
+                                 <div class="modify-buttons">
+                                    <button type="btn button" class="delete-button" id="delete-button" data-id=${car.id}>Delete</button>
+                                    <button type="btn button" id="edit-button">Edit</button>
+                                 </div>
                                 `;
                         };
                         let closeCarModal = () => {
                             carClick.style.display = "none";
                         }
+
                         carCard.addEventListener('click', openCarModal);
                         carClose.addEventListener('click', closeCarModal);
                     }
@@ -42,6 +51,17 @@ document.addEventListener('DOMContentLoaded', () => {
                 })
             })
     }
+
+    displayAllCars();
+
+    // deleteButton.addEventListener('click', () => {
+    //     fetch('http://localhost:3000/cars/' + id, {
+    //         method: 'DELETE',
+    //         headers: {
+    //             'Content-Type': 'application/json'
+    //         }
+    //     }).then(response => response.json()).then(data => console.log(data))
+    // });
 
     let getAllSportsCars = (categoryId) => {
         fetch('http://localhost:3000/cars')
@@ -74,8 +94,24 @@ document.addEventListener('DOMContentLoaded', () => {
         }).then(response => response.json()).then(car)
     }
 
-    carForm.addEventListener('submit', carPostForm);
-    displayAllCars();
-    getAllSportsCars(1);
+    carModal.addEventListener('click', (e) => {
+        if (e.target.classList.contains('delete-button')) {
+            const id = e.target.dataset.id;
+            deleteCarForm(id);
+        }
+    })
 
+    let deleteCarForm = (id) => {
+        fetch(`http://localhost:3000/cars/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(response => response.json()).then(data => console.log(data))
+    }
+
+
+
+    carForm.addEventListener('submit', carPostForm);
+    getAllSportsCars(1);
 });
