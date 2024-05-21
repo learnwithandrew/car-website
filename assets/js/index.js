ab9924a09c2ea63337d4b3865a6c094368545774
GithinjiKelvin <githinjikelvin59@gmail.com>
2024-05-22 00:42:54 +0300

    Edit function

--- assets/js/index.js
+++ assets/js/index.js
@@ -35,7 +35,7 @@ document.addEventListener('DOMContentLoaded', () => {
                                  </div>
                                  <div class="modify-buttons">
                                     <button type="btn button" class="delete-button" id="delete-button" data-id=${car.id}>Delete</button>
-                                    <button type="btn button" id="edit-button">Edit</button>
+                                    <button type="btn button" class="edit-button" id="edit-button" data-id=${car.id}>Edit</button>
                                  </div>
                                 `;
                         };
@@ -98,6 +98,9 @@ document.addEventListener('DOMContentLoaded', () => {
         if (e.target.classList.contains('delete-button')) {
             const id = e.target.dataset.id;
             deleteCarForm(id);
+        } else if (e.target.classList.contains('edit-button')) {
+            const id = e.target.dataset.id;
+            editCarForm(id);
         }
     })
 
@@ -109,9 +112,58 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         }).then(response => response.json()).then(data => console.log(data))
     }
+    
+    function editCarForm(id){
+        document.querySelector(".update-form").style.display="block";
+        document.getElementById('myModal').style.display="none";
 
+        fetch('http://localhost:3000/cars')
+            .then(response => response.json())
+            .then((data) => {
+                let cars = data;
+                
 
+                    let updateObj = cars.find(vehicle => vehicle.id === id);
+                    document.querySelector(".update_id").value = updateObj.id;
+                    document.querySelector(".ucategoryId").value = updateObj.categoryId;
+                    document.querySelector(".ucarName").value = updateObj.carName;
+                    document.querySelector(".ucarImage").value = updateObj.carImage;
+                    document.querySelector(".ucarDetails").value = updateObj.carDetails;
+                    
+               
+            })
+        
+    }
 
+    document.querySelector(".update-button").addEventListener("click", update)
+
+    function update(){
+        let id = parseInt(document.querySelector(".update_id").value);
+        let categoryId = document.querySelector(".ucategoryId").value;
+        let carName = document.querySelector(".ucarName").value;
+        let carImage = document.querySelector(".ucarImage").value;
+        let carDetails = document.querySelector(".ucarDetails").value;
+    
+        let updateObj = {id,categoryId, carName, carImage, carDetails};
+    
+        fetch(`http://localhost:3000/cars/${updateObj.id}`,{
+            method:'PATCH',
+            headers:{
+                'Content-Type': 'application/json'
+            },
+            body:JSON.stringify(updateObj)
+        })
+        .then(res => res.json())
+        .then(updateObj => console.log(updateObj))
+  
+                document.querySelector(".update-form").style.display="none";
+            document.getElementById('myModal').style.display="none";
+    
+    }
+
+    
     carForm.addEventListener('submit', carPostForm);
     getAllSportsCars(1);
 });
+
+
